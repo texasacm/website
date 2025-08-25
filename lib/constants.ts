29449819d4d16dfe1ffb0db08364470c931f943b
Gabriel Keller <gabrieljameskeller@gmail.com>
2025-08-25 17:20:55 -0500

    New IM and OO application hotlinks

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
 export const SIGNIN_URL = 'https://utexas.qualtrics.com/jfe/form/SV_b47et3hfLcKFk6W';
+export const OO_APPLICATION_URL = 'https://forms.gle/UfXmzapyw9rYGn6t8';
+export const IM_APPLICATION_URL = 'https://forms.gle/M8wQEumurioESSfi7';
