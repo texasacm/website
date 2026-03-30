d102683d495637c508a13c99fe3a8a80e4e95a7c
Casey Charleston <caseycharleston@gmail.com>
2026-03-30 15:49:28 -0500

    make the imgs actual pngs

--- components/cs-week/cs-week-info.tsx
+++ components/cs-week/cs-week-info.tsx
@@ -5,13 +5,13 @@ export default function CSWeekInfo() {
     return (
         <section className="bg-[#FCF1DB]">
             <Header hideHeader={true} />
-            <div className="text-center container px-4 pt-12 sm:px-6 lg:px-8">
+            <div className="container px-4 pt-12 text-center sm:px-6 lg:px-8">
                 <h1 className="mb-4 font-pagkaki text-6xl font-bold text-[#50241A]">
                     What is CS Week?
                 </h1>
             </div>
             <Image
-                src={'/cs-week/turtle-on-log.PNG'}
+                src={'/cs-week/assets/turtle-on-log.png'}
                 alt="Turtle on Log"
                 width={600}
                 height={400}
