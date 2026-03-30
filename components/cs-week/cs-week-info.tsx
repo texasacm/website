bb4125ed4d6e0f0aa16a3e6057074ca02fabebd0
Casey Charleston <caseycharleston@gmail.com>
2026-03-30 15:08:52 -0500

    block out schedule for cs week

--- components/cs-week/cs-week-info.tsx
+++ components/cs-week/cs-week-info.tsx
@@ -6,7 +6,9 @@ export default function CSWeekInfo() {
         <section className="bg-[#FCF1DB]">
             <Header hideHeader={true} />
             <div className="container px-4 pt-12 sm:px-6 lg:px-8">
-                <h1 className="mb-4 font-pagkaki text-6xl font-bold text-[#50241A]">What is CS Week?</h1>
+                <h1 className="mb-4 font-pagkaki text-6xl font-bold text-[#50241A]">
+                    What is CS Week?
+                </h1>
             </div>
             <Image
                 src={'/cs-week/turtle-on-log.PNG'}
--- components/cs-week/cs-week-schedule.tsx
+++ components/cs-week/cs-week-schedule.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/header';
+import { Card, CardTitle } from '../ui/card';
 
 export default function CSWeekSchedule() {
     return (
@@ -6,6 +7,43 @@ export default function CSWeekSchedule() {
             <Header hideHeader={true} />
             <div className="container px-4 pt-12 sm:px-6 lg:px-8">
                 <h1 className="mb-4 font-pagkaki text-6xl font-bold text-white">Schedule</h1>
+                <div className="grid grid-cols-5 gap-4 p-8">
+                    <Card className="col-span-1 p-8">
+                        <CardTitle className="font-bold">Monday, April 13th</CardTitle>
+                        <p className="mt-2 text-gray-700">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </Card>
+                    <Card className="col-span-1 p-8">
+                        <CardTitle className="font-bold">Tuesday, April 14th</CardTitle>
+                        <p className="mt-2 text-gray-700">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </Card>
+                    <Card className="col-span-1 p-8">
+                        <CardTitle className="font-bold">Wednesday, April 15th</CardTitle>
+                        <p className="mt-2 text-gray-700">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </Card>
+                    <Card className="col-span-1 p-8">
+                        <CardTitle className="font-bold">Thursday, April 16th</CardTitle>
+                        <p className="mt-2 text-gray-700">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </Card>
+                    <Card className="col-span-1 p-8">
+                        <CardTitle className="font-bold">Friday, April 17th</CardTitle>
+                        <p className="mt-2 text-gray-700">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </Card>
+                </div>
             </div>
         </section>
     );
